084f14ae063fbe54acdfb1d98397d6c1329fa61a
Charansaimsc <charanmsc7969@gmail.com>
2026-01-17 12:37:32 +0530

    [charan]Add. Main Method file for map interface

---
+++ collections/MapInterface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+namespace mapinterface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        //     string filePath = "input.txt";
+
+        // WordFrequency counter = new WordFrequency();
+        // counter.CountWords(filePath);
+
+Dictionary<string, int> input = new Dictionary<string, int>()
+        {
+            { "A", 1 },
+            { "B", 2 },
+            { "C", 1 }
+        };
+
+        Dictionary<int, List<string>> result =
+            DictionaryInverter.Invert(input);
+
+        foreach (var pair in result)
+        {
+            Console.Write(pair.Key + " = [ ");
+            foreach (var value in pair.Value)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("]");
+        }
+    
+
+
+
+
+
+        }
+    }
+}
